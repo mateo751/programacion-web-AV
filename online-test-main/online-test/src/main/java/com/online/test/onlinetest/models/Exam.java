6532190a94f0e50a1ef78b16eb042720f6985deb
MATEO SEBASTIAN ENRIQUEZ LOPEZ <67690982+mateo751@users.noreply.github.com>
2022-05-13 13:31:22 -0500

    models

---
+++ online-test-main/online-test/src/main/java/com/online/test/onlinetest/models/Exam.java
@@ -0,0 +1,43 @@
+package com.online.test.onlinetest.models;
+
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name="TBL_EXAMS")
+@Getter
+@Setter
+
+public class Exam {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name ="TITLE",nullable = false )
+    private String title;
+
+    @Column(name ="DESCRIPTION" )
+    private String description;
+
+    @Column(name ="TIME_LIMIT" )
+    private int timeLimit;
+
+    @Column(name ="MINIMUM_PASSING_SCORE", precision = 4, scale = 2)
+    private double minimumPassingScore;
+
+    @Column(name ="NUMBER_OF_QUESTION")
+    private int numberOfQuestion;
+    
+    @Column(name ="INSTRUCTIONS" )
+    private String instructions;
+
+}
---
+++ online-test-main/online-test/src/main/java/com/online/test/onlinetest/models/Question.java
@@ -0,0 +1,33 @@
+package com.online.test.onlinetest.models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name="TBL_QUESTION")
+@Getter
+@Setter
+
+public class Question {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name ="NAME" )
+    private String name;
+
+    @Column(name ="DESCRIPTION" )
+    private String description;
+
+    @Column(name ="SCORE" )
+    private String score;
+
+    
+}
