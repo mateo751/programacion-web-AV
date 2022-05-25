139a7686f690d6a3a27a969314d664dec4542ffd
mateo751 <67690982+mateo751@users.noreply.github.com>
2022-05-25 14:25:48 -0500

    dto,new dto

---
+++ online-test-main/online-test/src/main/java/com/online/test/onlinetest/dto/ExamDTO.java
@@ -0,0 +1,12 @@
+package com.online.test.onlinetest.dto;
+
+
+public class ExamDTO {
+    private Long id;
+    private String title;
+    private String description;
+    private short timeLimit;
+    private double minimumPassingScore;
+    private short numberOfQuestions;
+    private String instructions;      
+}
---
+++ online-test-main/online-test/src/main/java/com/online/test/onlinetest/dto/NewExamDTO.java
@@ -0,0 +1,11 @@
+package com.online.test.onlinetest.dto;
+
+
+public class NewExamDTO {
+    private String title;
+    private String description;
+    private short timeLimit;
+    private double minimumPassingScore;
+    private short numberOfQuestions;
+    private String instructions;      
+}
